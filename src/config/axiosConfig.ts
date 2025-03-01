d6d54808795e69f953b11dba920b72b80a28b8c1
flcristian <qflorescucristian@gmail.com>
2025-03-01 20:12:10 +0200

    fix: api connection

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const API_PORT = import.meta.env.VITE_API_PORT;
 const API_VERSION = import.meta.env.VITE_API_VERSION;
-const BASE_URL = `http://localhost:${API_PORT}/api/${API_VERSION}/`;
+const API_ADDRESS = import.meta.env.VITE_API_ADDRESS;
+const BASE_URL = `http://${API_ADDRESS}:${API_PORT}/api/${API_VERSION}/`;
 
 const instance = axios.create({
   baseURL: BASE_URL,
